a13bbbd722ff63d04f0ab786f5e7c4ecc0ef5262
digaosss70 <53195134+digaosss70@users.noreply.github.com>
2025-03-12 22:21:00 -0300

    Update funcoes_manipular_arquivos.py

--- rest/python/funcoes_manipular_arquivos.py
+++ rest/python/funcoes_manipular_arquivos.py
@@ -11,7 +11,6 @@ load_dotenv()
 
 QLIK_HOST = os.getenv("HOST")
 QLIK_API_KEY = os.getenv("API_KEY")
-GITH_API_KEY = os.getenv("GHA_SCRT")
 
 qHeader = headers={"Authorization": f"Bearer {QLIK_API_KEY}", "Content-Type": "application/json"}
 qHeader2 = headers={"Authorization": f"Bearer {QLIK_API_KEY}"}
@@ -62,9 +61,6 @@ def createItem(itemName,folder,connectionId):
     response = requests.post(url, headers=qHeader2, files=files)
     return response.status_code
 
-def getItemPath(itemName,folder):
-    return os.path.join(folder, itemName)
-
 def updateItem(itemId,itemName,folder,connectionId):
     filePath = os.path.join(folder, itemName)
     url = f"{QLIK_HOST}/api/v1/data-files/{itemId}"
@@ -82,36 +78,3 @@ def deleteItem(itemId):
 
     response = requests.delete(url, headers=qHeader2)
     return response.status_code   
-
-def filesList():
-    mainFolder = Path('./toCopy')
-    filesInfo = []
-
-    for file in mainFolder.rglob('*'):
-        if file.is_file():
-            fileInfo = {
-                'file': file.name,
-                'folder': str(file.parent),
-                'folderSpace': str(file.parent)[7:],
-                'updateDate': datetime.fromisoformat(str(datetime.fromtimestamp(file.stat().st_mtime))[0:19])
-            }
-            filesInfo.append(fileInfo)
-    
-    return filesInfo
-
-def get_last_commit_date(repo, file_path):
-    url = f"https://api.github.com/repos/{repo}/commits"
-    params = {'path': file_path}
-    headers = {'Authorization': f'token {GITH_API_KEY}'}
-
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code == 200:
-        commits = response.json()
-        if commits:
-            dt = commits[0]['commit']['committer']['date']
-            last_commit_date = datetime.fromisoformat(str(datetime.strptime(dt, "%Y-%m-%dT%H:%M:%SZ"))[0:19]) 
-            return last_commit_date
-    return None
-
-
