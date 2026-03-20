bf639b0184b2bfddcc9ea7842c8fb16e2ae0543e
Rodrigo Soares da Silva <53195134+digaosss70@users.noreply.github.com>
2026-03-20 14:43:21 +0000

    add rest pyhton exportApp.py

---
+++ rest/python/exportaApp.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from datetime import datetime, timedelta
+import time
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+QLIK_HOST = os.getenv("host")
+QLIK_API_KEY = os.getenv("api_key")
+
+qHeader = headers={"Authorization": f"Bearer {QLIK_API_KEY}", "Content-Type": "application/json"}
+qHeader2 = headers={"Authorization": f"Bearer {QLIK_API_KEY}"}
+
+
+def exportaApp(appId):
+    url = f"{QLIK_HOST}/api/v1/apps/{appId}/export"
+    response = requests.post(url, headers=qHeader2, timeout=240)
+    print(response.status_code)
+    print(response.text)
+
+
+
+exportaApp('appId')
